8076fa5c5f9d9e383d4265b0ee3dedcc66695ef6
AHMAD <aua321890@gmail.com>
2023-05-20 20:41:34 +0700

    edit profile

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { router } = require("./src/routes");
+const { router } = require("./src/routes/index");
 const app = express();
+const helmet = require("helmet");
+
 const port = 5000;
 
 app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(router);
+app.use(helmet);
 
 app.listen(port, () => console.log(`serve on port: ${port}`));
 
--- src/model/user_model.js
+++ src/model/user_model.js
@@ -33,6 +33,29 @@ const userModel = {
       );
     });
   },
+
+  updateProfile: (id, fullname, email, phone, city, address, post_code) => {
+    return new Promise((resolve, reject) => {
+      DB.query(
+        `UPDATE users SET fullname = '${fullname}', email = '${email}', phone = '${phone}', city='${city}', address = '${address}', post_code = ${post_code}  WHERE id = '${id}'`,
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
+  updatePhoto: (id, { photo }) => {
+    return new Promise((resolve, reject) => {
+      DB.query(
+        `UPDATE users SET photo = '${photo}' WHERE id = '${id}'`,
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  },
 };
 
 module.exports = userModel;

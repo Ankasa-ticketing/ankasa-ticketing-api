6988b5fa4346f480f3ae916c989a37433a9672b6
kokolopo <kokolopo321@gmail.com>
2023-05-23 02:19:03 +0700

    review

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const userController = require("../controller/userController");
-const router = express.Router();
 const { fetchUsers, register, login, editProfile, editPhoto } = userController;
 const { validate } = require("../middleware/validation");
-const { registerSchema, loginSchema } = require("../helper/validationSchema");
+const {
+  registerSchema,
+  loginSchema,
+  updateProfileSchema,
+} = require("../helper/validationSchema");
 const upload = require("../middleware/uploadImage");
+const { verifyToken } = require("../middleware/verifyToken");
+
+const router = express.Router();
 
 router.get("/", fetchUsers);
 router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
-router.put("/users", editProfile);
+router.put("/users", verifyToken, validate(updateProfileSchema), editProfile);
 router.put("/users_photo", upload, editPhoto);
 
 module.exports = { router };
